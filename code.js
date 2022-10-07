80ab6141462e1f1e20532a5c821b7bab0d84514c
Soumya Singh <83289414+soumya12392@users.noreply.github.com>
2022-10-07 21:00:25 +0530

    Add files via upload

--- code.js
+++ code.js
@@ -1,4 +1,6 @@
 var img = "";
+var status = "";
+var objectDetector = "";
 
 function preload() {
     img = loadImage("dog_cat.jpg");
@@ -7,13 +9,37 @@ function preload() {
 function setup() {
     var canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
 }
 
 function draw() {
     image(img, 0, 0, 640, 420);
+
     fill("#FF0000");
     text("Dog", 45, 75);
     noFill();
     stroke("#FF0000");
     rect(30, 60, 450, 350);
+
+    fill("#FF0000");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+    }
 }
