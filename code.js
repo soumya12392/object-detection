588ad5493f2a4bb89a4b0801ff183ad90fe54958
Soumya Singh <83289414+soumya12392@users.noreply.github.com>
2022-10-10 19:31:43 +0530

    Add files via upload

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 var img = "";
 var status = "";
 var objectDetector = "";
+var objects = [];
 
 function preload() {
     img = loadImage("dog_cat.jpg");
@@ -16,17 +17,18 @@ function setup() {
 function draw() {
     image(img, 0, 0, 640, 420);
 
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
 
-    fill("#FF0000");
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#FF0000");
-    rect(300, 90, 270, 320);
+            fill("#FF0000");
+            var percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function modelLoaded() {
@@ -42,4 +44,5 @@ function gotResults(error, results) {
     else {
         console.log(results);
     }
+    objects = results;
 }
